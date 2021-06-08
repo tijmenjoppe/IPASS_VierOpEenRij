a78f951478bd4dd940124d266221366277d8b01d
Debug-Sam <74404094+Debug-Sam@users.noreply.github.com>
2021-06-08 11:15:22 +0200

    Het spel laat nu de goede speler zien, maar plaatst het stuk niet onderaan het bord. dit komt doordat de np matrix waarschijnlijk wordt omgedraaid of dat pygame's x en y as hier tegen werken.

--- domain/game.py
+++ domain/game.py
@@ -1,7 +1,8 @@
 from layout import board
-from layout import rules
+
 import pygame as pg
 import sys
+import numpy as np
 from pygame.locals import *
 import time
 
@@ -9,15 +10,47 @@ class Game:
 
     def __init__(self):
         self.bd = board.Board()
-        self.rl = rules.Rules()
+        self.board = np.zeros((6, 7))
+        self.player = 0
+
+    def player(self):
+        ''' maak 2 spelers '''
+        pass
+
+    def win(self):
+        ''' wie wint er en hoe kun je winnen'''
+        pass
+
+    def player_click(self, posx, posy, board, player):
+        if posx < 100:
+            self.move(player, 0, self.last_open_position(board, 1))
+        elif posx < 200 and posx > 100:
+            self.move(player, 1, self.last_open_position(board, 2))
+        elif posx < 300 and posx > 200:
+            self.move(player,2, self.last_open_position(board, 3))
+        elif posx < 400 and posx > 300:
+            self.move(player, 3, self.last_open_position(board, 4))
+        elif posx < 500 and posx > 400:
+            self.move(player, 4, self.last_open_position(board, 5))
+        elif posx < 600 and posx > 500:
+            self.move(player, 5, self.last_open_position(board, 6))
+
+    def last_open_position(self, board, x):
+        for i in board[x]:
+            if i == 1 or i == 2:
+                continue
+            else:
+                return i
+
+    def move(self, player, x, y):
+        print(player, x, y)
+        self.board[int(y)][x] = player
 
     def connect_four_game(self):
-        board = self.bd.board()
-        player = None
+
         pg.init()
         self.bd.first_screen()
-
-        self.bd.draw_board(board, player)
+        self.bd.draw_board(self.board, self.player)
         pg.display.update()
 
         while True:
@@ -26,4 +59,23 @@ class Game:
                 if event.type == QUIT:
                     pg.quit()
                     sys.exit()
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    posx, posy = pg.mouse.get_pos()
+                    if self.player == 1:
+                        self.bd.print_board(self.board)
+                        self.player_click(posx, posy, self.board, self.player )
+
+                        #red
+                        self.bd.draw_board(self.board, self.player)
+                    else:
+                        self.player = 2
+                        self.player_click(posx, posy, self.board, self.player)
+                        #yellow
+                        self.bd.draw_board(self.board, self.player )
+
+                    self.player += 1
+                    self.player = self.player % 2
+
+
+
 
--- domain/layout/board.py
+++ domain/layout/board.py
@@ -16,6 +16,8 @@ class Board:
         self.width = 700
         self.gameDisplay = pg.display.set_mode((self.height, self.width))
 
+    def print_board(self, board):
+        print( np.flip( board, 0 ) )
 
     def first_screen(self):
         initiating_window = pg.image.load("vieropeenrijcoveredit.png")
@@ -26,12 +28,7 @@ class Board:
         time.sleep( 3 )
         self.gameDisplay.fill(self.black)
 
-    def board(self):
-        board = np.zeros((6,7))
-        return board
-
     def draw_board(self, board, player):
-
         for i in range(7):
             for j in range(6):
                 x = i * 100 + 100 / 2
@@ -47,9 +44,6 @@ class Board:
             if player == 2:
                 pg.draw.circle( self.gameDisplay, self.yellow, (x, y), 100 / 2 - 5 )
 
-
-
-
         for i in range(7):
             for j in range(6):
                 x = i * 100 + 100 / 2
@@ -58,8 +52,6 @@ class Board:
                     pg.draw.circle(self.gameDisplay, self.red, (x, y), 100 / 2 - 5)
                 if board[j][i] == 2:
                     pg.draw.circle( self.gameDisplay, self.yellow, (x, y), 100 / 2 - 5)
-
-
 
         pg.display.update()
 
