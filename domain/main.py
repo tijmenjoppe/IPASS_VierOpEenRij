6dec2ab142ba4113e0629efb86c5d7642262b018
Tijmen Muller <tijmen.muller@hu.nl>
2022-03-14 14:06:06 +0100

    Added working directory to file path. Added background cover image.

--- domain/main.py
+++ domain/main.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+print(f"Adding '{dir}' to the file path.")
+sys.path.insert(0, dir)
+
 from domain.rules import game_loops
 
 connect_game = game_loops.Game_Loops()
